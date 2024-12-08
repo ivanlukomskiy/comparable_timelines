0d0e18b33def6488f11eee1bd7ba3bbdb2b748df
ivanlukomskiy <ivanlukomskiy@avride.ai>
2024-12-08 13:37:47 -0300

    generate some data

---
+++ gatherer/gather.py
@@ -0,0 +1,69 @@
+import hashlib
+import json
+import os
+
+import yaml
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI()
+
+responses_folder = "response-cache"
+requests_folder = "requests"
+
+def answer(prompt):
+    return client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            # {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    ).choices[0].message.content
+
+def query(prompt):
+    hash_object = hashlib.md5(prompt.encode()).hexdigest()
+    path = f"{responses_folder}/{hash_object}.txt"
+    if os.path.exists(path):
+        with open(path, "r") as file:
+            return file.read()
+    print('re-generating response')
+    response = answer(prompt)
+    with open(path, "w") as file:
+        file.write(response)
+    return response
+
+
+if __name__ == '__main__':
+    exploration = dict()
+
+    with open("prompts/exploration.txt", "r") as file:
+        high_scale_prompt = file.read()
+    with open("prompts/exploration-detailed.txt", "r") as file:
+        events_prompt = file.read()
+    high_scale_yaml = query(high_scale_prompt)
+    periods = yaml.safe_load(high_scale_yaml)
+    for period in periods['periods']:
+        prompt = (events_prompt
+                  .replace("$period", period['title'])
+                  .replace("$centuries", period['time']))
+        events_yaml = query(prompt)
+        events_src = yaml.safe_load(events_yaml)['events']
+        events = []
+        for event in events_src:
+            events.append({
+                'title': event['title'],
+                'timeStart': str(event.get('timeStart')),
+                'timeEnd': str(event.get('timeEnd')),
+                'precision': str(event.get('precision')),
+            })
+        exploration[period['title']] = events
+
+    with open("exploration.json", "w") as json_file:
+        print(exploration)
+        json.dump(exploration, json_file, indent=4)
+
+
--- src/canvas/fpsMeter/fpsMeter.ts
+++ src/canvas/fpsMeter/fpsMeter.ts
@@ -2,7 +2,7 @@ import {$rendersList, $totalRenders} from "../../stores/debug.ts";
 
 export function renderFpsMeter(x:number, y:number, ctx: CanvasRenderingContext2D) {
     ctx.font = `32px Arial`;
-    const total = $totalRenders.get();
+    // const total = $totalRenders.get();
     const last = $rendersList.get().length;
-    ctx.fillText(last + " FPS, " + total, x, y);
+    ctx.fillText(last + " FPS", x, y);
 }
